28b84c6929c1fca905b9370654982cbec8f6a05c
Narek Gevorkyan <mrnarek149@gmail.com>
2020-07-10 16:51:49 +0400

    Add BottomNavigation and Toolbar

--- app/src/main/java/com/cyclone/simbirsoftprobation/View/MainActivity.kt
+++ app/src/main/java/com/cyclone/simbirsoftprobation/View/MainActivity.kt
@@ -1,7 +1,10 @@
-package com.cyclone.simbirsoftprobation
+package com.cyclone.simbirsoftprobation.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.TEXT_ALIGNMENT_CENTER
+import com.cyclone.simbirsoftprobation.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/cyclone/simbirsoftprobation/View/SplashActivity.kt
+++ app/src/main/java/com/cyclone/simbirsoftprobation/View/SplashActivity.kt
@@ -1,9 +1,10 @@
-package com.cyclone.simbirsoftprobation
+package com.cyclone.simbirsoftprobation.View
 
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.cyclone.simbirsoftprobation.R
 
 class SplashActivity : AppCompatActivity(R.layout.splash_screen) {
     override fun onCreate(savedInstanceState: Bundle?) {
