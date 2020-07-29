443523d7499a40ff7382ecf6ce68d69d154a3ddf
Narek Gevorkyan <mrnarek149@gmail.com>
2020-07-30 00:14:56 +0400

    Реализован IntentService

--- app/src/main/java/com/cyclone/simbirsoftprobation/Presenter/JsonHelperIntentService.kt
+++ app/src/main/java/com/cyclone/simbirsoftprobation/Presenter/JsonHelperIntentService.kt
@@ -1,22 +1,43 @@
 package com.cyclone.simbirsoftprobation.Presenter
 
 import android.app.IntentService
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
 
 class JsonHelperIntentService : IntentService("BackgroundIntentService") {
 
+    companion object {
+        val ACTION = "LOAD_FILES"
+        val EXTRA_KEY_OUT = "EXTRA_OUT"
+        val finished = "finished"
+    }
+
+    lateinit var adapter: NewsAdapter
+
     fun start(
-        recyclerView: RecyclerView,
+        adapter: NewsAdapter,
         context: Context
     ) {
+        this.adapter = adapter
         val intentService = Intent(context, JsonHelperIntentService::class.java)
         context.startService(intentService)
-        recyclerView.adapter = NewsAdapter()
     }
 
     override fun onHandleIntent(intent: Intent?) {
+//        Thread.sleep(5000)
         Datas.events = JsonHelper(this).getEvents()
+        val response = Intent()
+        response.action = ACTION
+        response.addCategory(Intent.CATEGORY_DEFAULT)
+        response.putExtra(EXTRA_KEY_OUT, finished)
+        sendBroadcast(response)
+    }
+}
+
+class MyBroadcastReceiver(val recyclerView: RecyclerView) : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        recyclerView.adapter = NewsAdapter()
     }
 }
--- app/src/main/java/com/cyclone/simbirsoftprobation/View/NewsFragment.kt
+++ app/src/main/java/com/cyclone/simbirsoftprobation/View/NewsFragment.kt
@@ -1,19 +1,30 @@
 package com.cyclone.simbirsoftprobation.View
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.cyclone.simbirsoftprobation.Presenter.JsonHelperExecutor
-import com.cyclone.simbirsoftprobation.Presenter.JsonHelperIntentService
+import androidx.recyclerview.widget.RecyclerView
+import com.cyclone.simbirsoftprobation.Presenter.*
 import com.cyclone.simbirsoftprobation.R
 import kotlinx.android.synthetic.main.news_fragment.view.*
 
+//lateinit var recyclerView: RecyclerView
+
 class NewsFragment : Fragment(R.layout.news_fragment) {
+
+    lateinit var broadcastReceiver: MyBroadcastReceiver
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         view.news_recycler.layoutManager = LinearLayoutManager(context)
+        view.news_recycler.adapter = NewsAdapter()
 
         // Async
 //        JsonHelperAsync(view.context, view.news_recycler).execute()
@@ -22,7 +33,7 @@ class NewsFragment : Fragment(R.layout.news_fragment) {
 //        JsonHelperExecutor().submit(view.context, view.news_recycler)
 
         // IntentService
-        JsonHelperIntentService().start(view.news_recycler, view.context)
+//        JsonHelperIntentService().start(view.news_recycler.adapter as NewsAdapter, view.context)
 
         Toast.makeText(context, "toast", Toast.LENGTH_SHORT).show()
         view.filter.setOnClickListener {
@@ -30,10 +41,21 @@ class NewsFragment : Fragment(R.layout.news_fragment) {
                 ?.replace(R.id.main_view_fragment, FilterFragment())?.addToBackStack("filter")
                 ?.commit()
         }
+
+        broadcastReceiver = MyBroadcastReceiver(view.news_recycler)
+        val intentFilter = IntentFilter(JsonHelperIntentService.ACTION)
+        intentFilter.addCategory(Intent.CATEGORY_DEFAULT)
+        context?.registerReceiver(broadcastReceiver, intentFilter)
     }
 
     override fun onResume() {
         super.onResume()
         view?.news_recycler?.adapter?.notifyDataSetChanged()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        context?.unregisterReceiver(broadcastReceiver)
+    }
+
 }
