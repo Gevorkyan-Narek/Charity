87713c784e43729fc9310cc704ed957ef1a37cf2
Narek Gevorkyan <mrnarek149@gmail.com>
2020-07-31 13:55:17 +0400

    Реализована кнопка Войти Улучшен xml код в news_detail.xml

--- app/src/main/java/com/cyclone/simbirsoftprobation/View/AuthorizationActivity.kt
+++ app/src/main/java/com/cyclone/simbirsoftprobation/View/AuthorizationActivity.kt
@@ -1,12 +1,43 @@
 package com.cyclone.simbirsoftprobation.View
 
-import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.cyclone.simbirsoftprobation.R
+import com.jakewharton.rxbinding.widget.RxTextView
+import kotlinx.android.synthetic.main.activity_authorization.*
+import rx.Observable
 
 class AuthorizationActivity : AppCompatActivity(R.layout.activity_authorization) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Observable.combineLatest(
+            RxTextView.textChanges(input_email),
+            RxTextView.textChanges(input_password)
+        ) { t1, t2 -> t1.length > 5 && t2.length > 5 }
+            .subscribe {
+                if (it) {
+                    enter.isEnabled = true
+                    enter.setBackgroundColor(
+                        resources.getColor(
+                            R.color.colorPrimaryGreen,
+                            theme
+                        )
+                    )
+                } else {
+                    enter.isEnabled = false
+                    enter.setBackgroundColor(
+                        resources.getColor(
+                            R.color.dark_gray,
+                            theme
+                        )
+                    )
+                }
+            }
+
+        enter.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
