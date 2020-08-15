73f8c611bce37cc652a1968df6b4e0a064be3731
Narek Gevorkyan <mrnarek149@gmail.com>
2020-08-15 15:55:58 +0400

    Исправлены замечания

--- app/src/main/java/com/cyclone/simbirsoftprobation/json_helper/JsonHelperExecutor.kt
+++ app/src/main/java/com/cyclone/simbirsoftprobation/json_helper/JsonHelperExecutor.kt
@@ -31,45 +31,12 @@ class JsonHelperExecutor {
                 exception = e
             } finally {
                 if (!executor.isShutdown) executor.shutdownNow()
-                if (callback != null) {
-                    if (exception == null) {
-                        callback.onSuccess(Datas.events)
-                    } else callback.onFailure(exception)
-                }
+                if (exception == null) callback?.onSuccess(Datas.events)
+                else callback?.onFailure(exception)
             }
 
             true
 
         }.sendEmptyMessage(0)
     }
-//    fun submit(
-//        context: Context,
-//        newsRecycler: RecyclerView,
-//        progressBar: ProgressBar
-//    ) {
-//
-//        Handler {
-//            val executor = Executors.newSingleThreadExecutor()
-//
-//            executor.submit {
-//                Thread.sleep(5000)
-//                Datas.events = JsonHelper(context).getEvents()
-//            }
-//
-//            try {
-//                executor.shutdown()
-//                executor.awaitTermination(10, TimeUnit.SECONDS)
-//            } catch (e: InterruptedException) {
-//                e.printStackTrace()
-//            } finally {
-//                if (!executor.isShutdown) executor.shutdownNow()
-//                progressBar.visibility = View.GONE
-//                newsRecycler.adapter =
-//                    NewsAdapter()
-//            }
-//
-//            true
-//
-//        }.sendEmptyMessage(0)
-//    }
 }
--- app/src/main/java/com/cyclone/simbirsoftprobation/news/NewsFragment.kt
+++ app/src/main/java/com/cyclone/simbirsoftprobation/news/NewsFragment.kt
@@ -8,7 +8,10 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cyclone.simbirsoftprobation.R
 import com.cyclone.simbirsoftprobation.filter.FilterFragment
-import com.cyclone.simbirsoftprobation.json_helper.*
+import com.cyclone.simbirsoftprobation.json_helper.JsonHelperAsync
+import com.cyclone.simbirsoftprobation.json_helper.JsonHelperCallback
+import com.cyclone.simbirsoftprobation.json_helper.JsonHelperIntentService
+import com.cyclone.simbirsoftprobation.json_helper.MyBroadcastReceiver
 import com.cyclone.simbirsoftprobation.model.Event
 import kotlinx.android.synthetic.main.news_fragment.view.*
 
